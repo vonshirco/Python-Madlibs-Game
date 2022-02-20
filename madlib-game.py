ff0d7fca09554c335a60e7865d40c4af60b32153
Von <vonshirco.vs@gmail.com>
2022-02-20 18:26:21 +0300

    Update file

--- madlib-game.py
+++ madlib-game.py
@@ -28,7 +28,6 @@ def madlib1(): #Photographer madlib
     print('say ' + food + ', the photographer said as the camera flashed! ' + name + ' and I had gone to ' + place +' to get our photos taken today. The first photo we really wanted was a picture of us dressed as ' + animals + ' pretending to be a ' + profession + ' .when we saw the second photo, it was exactly what I wanted. We both looked like ' + things + ' wearing ' + cloth + ' and ' + verb + ' --exactly what I had in mind')
 
 
-
 def madlib2(): #Apple Madlib
    
     adjactive = input('enter adjective : ')
@@ -44,7 +43,6 @@ def madlib2(): #Apple Madlib
     print('Last night I dreamed I was a ' +adjactive+ ' butterfly with ' + color+ ' splocthes that looked like '+thing+ ' .I flew to ' + place+ ' with my bestfriend and '+person+ ' who was a '+adjactive1+ ' ' +insect +' .We ate some ' +food+ ' when we got there and then decided to '+verb+ ' and the dream ended when I said-- lets ' +verb+ '.')
     
 
-
 def madlib3(): #The butterfly madlib
     person = input('enter person name: ')
     color = input('enter color : ')
@@ -59,7 +57,6 @@ def madlib3(): #The butterfly madlib
 
    
     print('Today we picked apple from '+person+ "'s Orchard. I had no idea there were so many different varieties of apples. I ate " +color+ ' apples straight off the tree that tested like '+foods+ '. Then there was a '+adjective+ ' apple that looked like a ' + thing + '.When our bag were full, we went on a free hay ride to '+place+ ' and back. It ended at a hay pile where we got to ' +verb+ ' ' +adverb+ '. I can hardly wait to get home and cook with the apples. We are going to make appple '+food+ ' and '+things+' pies!.')  
-
 
 
 #Buttons - widget used to display buttons on our tkinter window  
